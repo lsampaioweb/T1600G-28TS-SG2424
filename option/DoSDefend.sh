b849ce6ffb0a151ae427746a9d2d11cb8544989b
Luciano Sampaio <lsampaioweb@gmail.com>
2022-06-29 23:47:57 -0300

    The function to enable DOS defend was commented. I need more studying.

--- option/DoSDefend.sh
+++ option/DoSDefend.sh
@@ -4,11 +4,11 @@
 # Author:       Luciano Sampaio 
 # Date:         29-Dec-2020
 
-#enableDoSDefend () {
+enableDoSDefend () {
   # As the option was dropping NFS packages, I had to disable the DOS defend function. 
   # The problematic rule was "port-less-1024", but I don't know if other rules are blocking other things.
 
-  # logInfo "Enabling DoS Defend."
+  logInfo "Enabling DoS Defend."
 
   # (
   #   sendEnable
@@ -21,5 +21,5 @@
   #   sendExit
   # ) | runSSH $USER_BOT@$DEVICE_IP
 
-  # logInfo "Finished."
-#}
+  logInfo "Finished."
+}
