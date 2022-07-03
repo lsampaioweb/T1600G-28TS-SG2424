a2c195a4512fa52905be0fdb93db7279c52b1c4d
Luciano Sampaio <lsampaioweb@gmail.com>
2022-07-03 17:55:18 -0300

    Fineshed the script that enables EEE on the individual e LACP interfaces.

--- option/EEE.sh
+++ option/EEE.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# Description:  Enable EEE (Energy Efficient Ethernet) on the ports.
+# Description:  Enable EEE (Energy Efficient Ethernet) on the interfaces.
 # Author:       Luciano Sampaio 
 # Date:         29-Dec-2020
 
 enableEEE () {
   logInfo "Enabling EEE."
 
-  expect -f option/EEEExpect.sh  "$USER_BOT" "$DEVICE_IP"
+  expect -f option/EEEExpect.sh  "$USER_BOT" "$DEVICE_IP" "$EEE_INTERFACES" "$EEE_LACPS"
 
   logInfo "Finished."
 }
--- option/EEEExpect.sh
+++ option/EEEExpect.sh
@@ -2,6 +2,8 @@
 
 set USER_BOT [lindex $argv 0]
 set DEVICE_IP [lindex $argv 1]
+set EEE_INTERFACES [lindex $argv 2]
+set EEE_LACPS [lindex $argv 3]
 
 set consoleMsg "T1600G-28TS"
 set lineBreak "\r"
@@ -10,9 +12,17 @@ spawn ssh $USER_BOT@$DEVICE_IP
 
 expect "${consoleMsg}>" { send "enable${lineBreak}" }
 expect "${consoleMsg}#" { send "config${lineBreak}" }
-expect "${consoleMsg}(config)#" { send "interface range gigabitEthernet 1/0/1-24${lineBreak}" }
+
+# Interfaces.
+expect "${consoleMsg}(config)#" { send "interface range gigabitEthernet ${EEE_INTERFACES}${lineBreak}" }
+expect "${consoleMsg}(config-if-range)#" { send "eee${lineBreak}" }
+expect "${consoleMsg}(config-if-range)#" { send "exit${lineBreak}" }
+
+# LACPs.
+expect "${consoleMsg}(config)#" { send "interface range port-channel ${EEE_LACPS}${lineBreak}" }
 expect "${consoleMsg}(config-if-range)#" { send "eee${lineBreak}" }
 expect "${consoleMsg}(config-if-range)#" { send "end${lineBreak}" }
+
 expect "${consoleMsg}#" { send "copy running-config startup-config${lineBreak}" }
 expect "${consoleMsg}#" { send "exit${lineBreak}" }
 expect "${consoleMsg}" { send "exit${lineBreak}" }
